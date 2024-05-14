e718bd51c55650678728155f7944810c2eca60e2
lusm <loveyousomuch554@gmail.com>
2024-05-14 15:58:39 +0300

    add popup url argument for main

--- main.py
+++ main.py
@@ -17,9 +17,11 @@ logger = logging.getLogger(__name__)
 def parse_cli_args():
   index_html_filepath = os.path.realpath(sys.argv[1])
   open_windows_cnt = int(sys.argv[2])
+  popup_url = sys.argv[3]
   logger.info(f'{open_windows_cnt=}')
   logger.info(f'{index_html_filepath=}')
-  return index_html_filepath, open_windows_cnt
+  logger.info(f'{popup_url=}')
+  return index_html_filepath, open_windows_cnt, popup_url
 
 def get_driver_path():
   driver_path = ChromeDriverManager().install()
@@ -36,11 +38,12 @@ def get_driver():
   return driver
 
 def main():
-  index_html_filepath, open_windows_cnt = parse_cli_args()
+  index_html_filepath, open_windows_cnt, popup_url = parse_cli_args()
   driver = get_driver()
   url = f'file://{index_html_filepath}'
   driver.get(url)
   driver.execute_script(f'window.windows_cnt = {open_windows_cnt}')
+  driver.execute_script(f'window.popup_url = {popup_url!r}')
   driver.execute_script('main()')
   time.sleep(40)
 
