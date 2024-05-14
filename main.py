417349e85413317d0de43d41c3fd2d2b14062f63
lusm <loveyousomuch554@gmail.com>
2024-05-14 13:00:45 +0300

    change driver from chomium to chrome

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions, ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
 import time
 import logging
 import sys
@@ -18,7 +17,7 @@ options = ChromeOptions()
 index_html_filepath = os.path.realpath(sys.argv[1])
 url = f'file://{index_html_filepath}'
 logger.info(f'{url=!r}')
-driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+driver_path = ChromeDriverManager().install()
 logger.info(f'{driver_path=!r}')
 driver = webdriver.Chrome(options=options)
 driver.get(url)
